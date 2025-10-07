1ed998e935d437e575667430066bae0e78b2b229
Luis Faria <lfariabr@gmail.com>
2025-10-08 06:53:02 +1100

    feat(header): implements it

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link'
 import Image from 'next/image'
 import NavItems from './NavItems'
+import UserDropdown from './UserDropdown'
 
 const Header = () => {
   return (
@@ -12,7 +13,7 @@ const Header = () => {
             <nav className="hidden sm:block">
                 <NavItems />
             </nav>
-        {/* User Dropdown */}
+            <UserDropdown />
         </div>
     </header>
   )
